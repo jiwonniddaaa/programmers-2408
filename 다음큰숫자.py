f0eb5012210ffcecab1402e6e672f433eeaa0b7b
jiwonniddaaa <jiwonniddaaa02@gmail.com>
2024-09-03 09:56:50 +0900

    while문 괄호빼기

--- 다음큰숫자.py
+++ 다음큰숫자.py
@@ -4,7 +4,7 @@ def solution(n):
     # n을 2진수로 변환했을 때 1의 개수 구하기
     one_count = bin(n)[2:].count('1')
     # while문으로 n+1부터 2진수로 변환하여 1의 개수 구하고 n의 1의 개수와 비교 -> 같으면 break
-    while (one_count != n_one_count) :
+    while one_count != n_one_count :
         n += 1
         n_one_count = bin(n)[2:].count('1')
     answer = n
