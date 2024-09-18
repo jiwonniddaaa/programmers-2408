4d82dac3c06f14aba9f52a81527c5493383d7d6b
jiwonniddaaa <jiwonniddaaa02@gmail.com>
2024-09-18 09:07:29 +0900

    행렬의 곱셈 풀이

---
+++ 행렬의 곱셈.py
@@ -0,0 +1,11 @@
+def solution(arr1, arr2):
+    answer = [[0] * len(arr2[0]) for index in range(len(arr1))]
+    
+    for i in range(len(arr1)):
+        for j in range(len(arr2[0])):
+            sum = 0
+            for k in range(len(arr2)):
+                sum += arr1[i][k] * arr2[k][j]
+            answer[i][j] = sum
+    
+    return answer
